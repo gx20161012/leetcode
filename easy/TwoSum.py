ca44bb9bfffe3ae3b895a9026f9881149e531ce0
gaoxiang's git <gaoxiang199703@hotmail.com>
2018-09-08 23:48:48 +0800

    modify TwoSum.py

--- easy/TwoSum.py
+++ easy/TwoSum.py
@@ -8,4 +8,24 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
+        result = []
+        for i in nums:
+            if target-i in nums and i != target-i:
+                result.append(nums.index(i))
+                result.append(nums.index(target-i))
+                break
+            elif target-i in nums and i == target-i and nums.count(i) >= 2:
+                index = nums.index(i)
+                result.append(index)
+                nums1 = nums
+                nums1[index] = 'a'
+                result.append(nums1.index(i))
+                break
+            else:
+                continue
+        return result
+nums = [3,3]
+target = 6
+s = Solution()
+print(s.twoSum(nums, target))
         
