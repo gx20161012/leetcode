1460b31f5e45c493127cca3354458b52175dae16
gaoxiang's git <gaoxiang199703@hotmail.com>
2018-09-16 14:53:11 +0800

    modify a problem---Sqrtx

--- easy/Sqrtx.py
+++ easy/Sqrtx.py
@@ -14,11 +14,11 @@ class Solution:
             return 0
         x1 = 0.0
         x2 = 1.0
-        while abs(x1 - x2) > 0.0001:
+        while abs(x1 - x2) > 0.0000000000001:
             x1 = x2
-            x2 = x1 // 2 + x // 2 * x1
+            x2 = x1 / 2 + x / (2 * x1)
 
-        return math.floor(x1)
+        return math.ceil(x1)
 s  = Solution()
-print(s.mySqrt(4))
+print(s.mySqrt(8))
             
