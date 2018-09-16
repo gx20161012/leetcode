0e2e46a510b9aa75dbde22e253138c27834e9bb0
gaoxiang's git <gaoxiang199703@hotmail.com>
2018-09-16 14:50:16 +0800

    add a new problem---Sqrtx

---
+++ easy/Sqrtx.py
@@ -0,0 +1,24 @@
+#Implement int sqrt(int x).
+#Compute and return the square root of x, 
+# where x is guaranteed to be a non-negative integer.
+#Since the return type is an integer, 
+#the decimal digits are truncated and only the integer part of the result is returned.
+import math
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x <= 0:
+            return 0
+        x1 = 0.0
+        x2 = 1.0
+        while abs(x1 - x2) > 0.0001:
+            x1 = x2
+            x2 = x1 // 2 + x // 2 * x1
+
+        return math.floor(x1)
+s  = Solution()
+print(s.mySqrt(4))
+            
